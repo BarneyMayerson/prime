a6949f050cdd9324e32e5db187278187c6fc6889
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-10-13 18:09:16 +0300

    Trying a couple primevue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
 import "./bootstrap";
 import "../css/app.css";
+import "../css/flags.css";
+import "primeicons/primeicons.css";
 
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
@@ -13,10 +17,15 @@ createInertiaApp({
   resolve: (name) =>
     resolvePageComponent(
       `./Pages/${name}.vue`,
-      import.meta.glob("./Pages/**/*.vue")
+      import.meta.glob("./Pages/**/*.vue"),
     ),
   setup({ el, App, props, plugin }) {
     return createApp({ render: () => h(App, props) })
+      .use(PrimeVue, {
+        theme: {
+          preset: Aura,
+        },
+      })
       .use(plugin)
       .use(ZiggyVue)
       .mount(el);
