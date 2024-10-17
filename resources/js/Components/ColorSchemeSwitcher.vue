f5afb9c5b24fb33cf82f018b4328a8a0a0ae1da1
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-10-17 15:07:48 +0300

    Setting the surface

--- resources/js/Components/ColorSchemeSwitcher.vue
+++ resources/js/Components/ColorSchemeSwitcher.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { ref } from "vue";
-import { updatePreset, updatePrimaryPalette } from "@primevue/themes";
+import { updatePreset, updateSurfacePalette } from "@primevue/themes";
 import Button from "primevue/button";
 import Popover from "primevue/popover";
 
 const op = ref();
 
 const selectedPrimaryColor = ref("emerald");
+const selectedSurfaceColor = ref("slate");
 const theme = ref("Aura");
 
 const primaryColors = ref([
@@ -267,22 +268,144 @@ const primaryColors = ref([
     },
   },
 ]);
-
-const orangePalette = () => {
-  updatePrimaryPalette({
-    50: "{orange.50}",
-    100: "{orange.100}",
-    200: "{orange.200}",
-    300: "{orange.300}",
-    400: "{orange.400}",
-    500: "{orange.500}",
-    600: "{orange.600}",
-    700: "{orange.700}",
-    800: "{orange.800}",
-    900: "{orange.900}",
-    950: "{orange.950}",
-  });
-};
+const surfaces = ref([
+  {
+    name: "slate",
+    palette: {
+      0: "#ffffff",
+      50: "#f8fafc",
+      100: "#f1f5f9",
+      200: "#e2e8f0",
+      300: "#cbd5e1",
+      400: "#94a3b8",
+      500: "#64748b",
+      600: "#475569",
+      700: "#334155",
+      800: "#1e293b",
+      900: "#0f172a",
+      950: "#020617",
+    },
+  },
+  {
+    name: "gray",
+    palette: {
+      0: "#ffffff",
+      50: "#f9fafb",
+      100: "#f3f4f6",
+      200: "#e5e7eb",
+      300: "#d1d5db",
+      400: "#9ca3af",
+      500: "#6b7280",
+      600: "#4b5563",
+      700: "#374151",
+      800: "#1f2937",
+      900: "#111827",
+      950: "#030712",
+    },
+  },
+  {
+    name: "zinc",
+    palette: {
+      0: "#ffffff",
+      50: "#fafafa",
+      100: "#f4f4f5",
+      200: "#e4e4e7",
+      300: "#d4d4d8",
+      400: "#a1a1aa",
+      500: "#71717a",
+      600: "#52525b",
+      700: "#3f3f46",
+      800: "#27272a",
+      900: "#18181b",
+      950: "#09090b",
+    },
+  },
+  {
+    name: "neutral",
+    palette: {
+      0: "#ffffff",
+      50: "#fafafa",
+      100: "#f5f5f5",
+      200: "#e5e5e5",
+      300: "#d4d4d4",
+      400: "#a3a3a3",
+      500: "#737373",
+      600: "#525252",
+      700: "#404040",
+      800: "#262626",
+      900: "#171717",
+      950: "#0a0a0a",
+    },
+  },
+  {
+    name: "stone",
+    palette: {
+      0: "#ffffff",
+      50: "#fafaf9",
+      100: "#f5f5f4",
+      200: "#e7e5e4",
+      300: "#d6d3d1",
+      400: "#a8a29e",
+      500: "#78716c",
+      600: "#57534e",
+      700: "#44403c",
+      800: "#292524",
+      900: "#1c1917",
+      950: "#0c0a09",
+    },
+  },
+  {
+    name: "soho",
+    palette: {
+      0: "#ffffff",
+      50: "#f4f4f4",
+      100: "#e8e9e9",
+      200: "#d2d2d4",
+      300: "#bbbcbe",
+      400: "#a5a5a9",
+      500: "#8e8f93",
+      600: "#77787d",
+      700: "#616268",
+      800: "#4a4b52",
+      900: "#34343d",
+      950: "#1d1e27",
+    },
+  },
+  {
+    name: "viva",
+    palette: {
+      0: "#ffffff",
+      50: "#f3f3f3",
+      100: "#e7e7e8",
+      200: "#cfd0d0",
+      300: "#b7b8b9",
+      400: "#9fa1a1",
+      500: "#87898a",
+      600: "#6e7173",
+      700: "#565a5b",
+      800: "#3e4244",
+      900: "#262b2c",
+      950: "#0e1315",
+    },
+  },
+  {
+    name: "ocean",
+    palette: {
+      0: "#ffffff",
+      50: "#fbfcfc",
+      100: "#F7F9F8",
+      200: "#EFF3F2",
+      300: "#DADEDD",
+      400: "#B1B7B6",
+      500: "#828787",
+      600: "#5F7274",
+      700: "#415B61",
+      800: "#29444E",
+      900: "#183240",
+      950: "#0c1920",
+    },
+  },
+]);
 
 const toggle = (event) => {
   op.value.toggle(event);
@@ -291,6 +414,8 @@ const toggle = (event) => {
 const updateColors = (type, color) => {
   if (type === "primary") {
     selectedPrimaryColor.value = color.name;
+  } else if (type === "surface") {
+    selectedSurfaceColor.value = color.name;
   }
 
   applyTheme(type, color);
@@ -299,6 +424,8 @@ const updateColors = (type, color) => {
 const applyTheme = (type, color) => {
   if (type === "primary") {
     updatePreset(getPresetExt());
+  } else if (type === "surface") {
+    updateSurfacePalette(color.palette);
   }
 };
 
@@ -410,6 +537,24 @@ const getPresetExt = () => {
             outlineColor: `${selectedPrimaryColor === primaryColor.name ? 'var(--p-primary-color)' : ''}`,
           }"
           @click="updateColors('primary', primaryColor)"
+        />
+      </div>
+
+      <span class="font-medium block text-sm">Surface colors</span>
+      <div
+        class="self-stretch justify-start items-start gap-2 inline-flex flex-wrap"
+      >
+        <button
+          type="button"
+          class="outline outline-2 outline-offset-1 outline-transparent cursor-pointer p-0 rounded-[50%] w-5 h-5"
+          v-for="surface in surfaces"
+          :key="surface.name"
+          :title="surface.name"
+          :style="{
+            backgroundColor: surface.palette['500'],
+            outlineColor: `${selectedSurfaceColor === surface.name ? 'var(--p-surface-500)' : ''}`,
+          }"
+          @click="updateColors('surface', surface)"
         />
       </div>
     </div>
