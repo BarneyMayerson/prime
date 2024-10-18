fba82f5c1e33d9b4dbf0dbf7f6bfbf39022e0ae7
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-10-18 15:30:15 +0300

    Browser localStorage for the purpose storing settings (part 1)

--- resources/js/Components/ThemeSwitcher.vue
+++ resources/js/Components/ThemeSwitcher.vue
@@ -1,15 +1,42 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import Button from "primevue/button";
 
+const theme = ref("light");
+const themes = ["light", "dark"];
 const iconClass = ref("pi-moon");
 
 const toggleDarkMode = () => {
   const root = document.getElementsByTagName("html")[0];
 
-  root.classList.toggle("p-dark");
-  iconClass.value = iconClass.value === "pi-moon" ? "pi-sun" : "pi-moon";
+  theme.value === "light" ? applyDark(root) : applyLight(root);
 };
+
+const applyDark = (el) => {
+  el.classList.add("p-dark");
+  iconClass.value = "pi-sun";
+  theme.value = "dark";
+  localStorage.theme = "dark";
+};
+
+const applyLight = (el) => {
+  el.classList.remove("p-dark");
+  iconClass.value = "pi-moon";
+  theme.value = "light";
+  localStorage.theme = "light";
+};
+
+onMounted(() => {
+  const root = document.getElementsByTagName("html")[0];
+
+  if (themes.includes(localStorage.theme)) {
+    theme.value = localStorage.theme;
+
+    if (theme.value === "dark") {
+      applyDark(root);
+    }
+  }
+});
 </script>
 <template>
   <Button
