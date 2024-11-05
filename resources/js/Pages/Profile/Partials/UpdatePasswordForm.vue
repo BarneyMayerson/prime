373b0e6b16053f6c03efcfac00877028837e8af4
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-11-05 10:56:52 +0300

    Modifying UpdatePassword form with new components

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -6,7 +6,9 @@ import FormSection from "@/Components/FormSection.vue";
 import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import Button from "primevue/button";
+import FloatLabel from "primevue/floatlabel";
 import InputText from "primevue/inputtext";
+import { useToast } from "primevue/usetoast";
 
 const passwordInput = ref(null);
 const currentPasswordInput = ref(null);
@@ -21,7 +23,15 @@ const updatePassword = () => {
   form.put(route("user-password.update"), {
     errorBag: "updatePassword",
     preserveScroll: true,
-    onSuccess: () => form.reset(),
+    onSuccess: () => {
+      form.reset();
+      toast.add({
+        severity: "success",
+        summary: "Success!",
+        detail: "Password has been updated.",
+        life: 3000,
+      });
+    },
     onError: () => {
       if (form.errors.password) {
         form.reset("password", "password_confirmation");
@@ -35,6 +45,8 @@ const updatePassword = () => {
     },
   });
 };
+
+const toast = useToast();
 </script>
 
 <template>
@@ -47,49 +59,51 @@ const updatePassword = () => {
 
     <template #form>
       <div class="col-span-6 sm:col-span-4">
-        <InputLabel for="current_password" value="Current Password" />
-        <InputText
-          id="current_password"
-          ref="currentPasswordInput"
-          v-model="form.current_password"
-          type="password"
-          class="mt-1 block w-full"
-          autocomplete="current-password"
-        />
+        <FloatLabel>
+          <InputText
+            id="current_password"
+            ref="currentPasswordInput"
+            v-model="form.current_password"
+            type="password"
+            class="mt-1 block w-full"
+            autocomplete="current-password"
+          />
+          <label for="current_password">Current Password</label>
+        </FloatLabel>
         <InputError :message="form.errors.current_password" class="mt-2" />
       </div>
 
-      <div class="col-span-6 sm:col-span-4">
-        <InputLabel for="password" value="New Password" />
-        <InputText
-          id="password"
-          ref="passwordInput"
-          v-model="form.password"
-          type="password"
-          class="mt-1 block w-full"
-          autocomplete="new-password"
-        />
+      <div class="col-span-6 mt-10 sm:col-span-4">
+        <FloatLabel>
+          <InputText
+            id="password"
+            ref="passwordInput"
+            v-model="form.password"
+            type="password"
+            class="mt-1 block w-full"
+            autocomplete="new-password"
+          />
+          <label for password>New Password</label>
+        </FloatLabel>
         <InputError :message="form.errors.password" class="mt-2" />
       </div>
 
-      <div class="col-span-6 sm:col-span-4">
-        <InputLabel for="password_confirmation" value="Confirm Password" />
-        <InputText
-          id="password_confirmation"
-          v-model="form.password_confirmation"
-          type="password"
-          class="mt-1 block w-full"
-          autocomplete="new-password"
-        />
+      <div class="col-span-6 mt-2 sm:col-span-4">
+        <FloatLabel>
+          <InputText
+            id="password_confirmation"
+            v-model="form.password_confirmation"
+            type="password"
+            class="mt-1 block w-full"
+            autocomplete="new-password"
+          />
+          <label for="password_confirmation">Confinm Password</label>
+        </FloatLabel>
         <InputError :message="form.errors.password_confirmation" class="mt-2" />
       </div>
     </template>
 
     <template #actions>
-      <ActionMessage :on="form.recentlySuccessful" class="me-3">
-        Saved.
-      </ActionMessage>
-
       <Button
         :class="{ 'opacity-25': form.processing }"
         :disabled="form.processing"
